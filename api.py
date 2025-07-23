4e42d471fdd6ff1652c2776a97381d9100aad09c
Mitul Rana <145378357+Mitulrana2910@users.noreply.github.com>
2025-07-23 19:07:02 +0530

    Update api.py

--- api.py
+++ api.py
@@ -2,7 +2,7 @@ from huggingface_hub import InferenceClient
 from transformers import pipeline
 def sent(text):
     client = InferenceClient(
-        token="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",  # your token
+        token="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",  # your token
     )
 
     text_input = text
@@ -47,7 +47,7 @@ def ner(text):
 
 def summarize_text(text):
     client = InferenceClient(
-        token="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",  # Replace with your Hugging Face token
+        token="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",  # Replace with your Hugging Face token
     )
 
     result = client.text_generation(
